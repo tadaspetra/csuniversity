bf6f254089d3121f941c7e4e3739ca1bad57fe13
Tadas Petra <connectwithamateur@gmail.com>
2020-12-01 09:38:57 -0600

    video #2

---
+++ 1_PythonForEverybody/2_Building Blocks/buildingblocks.py
@@ -0,0 +1,27 @@
+print("CS UNIVERSITY")
+print("-------------")
+# messy variable names, but it works
+# saldfsdfa = 20
+# jhwqbnower = 5
+# wqsadfasd = saldfsdfa + jhwqbnower
+# print(wqsadfasd)
+
+# better variable names, and still works
+# price = 20
+# tax = 5
+# total = price + tax
+# print(total)
+
+# converting data types
+# x = "1"
+# y = int(x) + 1
+# print(y)
+
+# user input
+# name = input("What is your name? ")
+# print("Hello", name)
+
+# converting celcius to fahrenheit
+celcius = input("What is the temprature in Celcius? ")
+fahrenheit = float(celcius)*(9/5) + 32
+print(fahrenheit)
